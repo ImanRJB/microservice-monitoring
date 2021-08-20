1947cc43eb522a245c1fc1b9147f6540dcdaf792
Iman Rajabi <iman.rjb@gmail.com>
2021-08-21 01:25:21 +0430

    change seeder

--- database/seeders/MicroserviceSeeder.php
+++ database/seeders/MicroserviceSeeder.php
@@ -30,8 +30,8 @@ class MicroserviceSeeder extends Seeder
             ['name' => 'ml_ticket', 'hostname' => 'http://127.0.0.1', 'port' => 11100, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
             ['name' => 'ml_balance', 'hostname' => 'http://127.0.0.1', 'port' => 8027, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
             ['name' => 'ml_home', 'hostname' => 'http://127.0.0.1', 'port' => 8028, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
-            ['name' => 'ml_ipg', 'hostname' => 'http://127.0.0.1', 'port' => 8029, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
-            ['name' => 'ml_fastpay', 'hostname' => 'http://127.0.0.1', 'port' => 8030, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
+            ['name' => 'ml_ipg', 'hostname' => 'http://127.0.0.1', 'port' => 11500, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
+            ['name' => 'ml_fastpay', 'hostname' => 'http://127.0.0.1', 'port' => 11600, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
             ['name' => 'ml_withdrawfile', 'hostname' => 'http://127.0.0.1', 'port' => 8031, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
             ['name' => 'ml_tax', 'hostname' => 'http://127.0.0.1', 'port' => 8032, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
             ['name' => 'ml_shaparak', 'hostname' => 'http://127.0.0.1', 'port' => 8033, 'consumer_logs' => 0, 'consumer_error_logs' => 0, 'status' => 0],
@@ -40,8 +40,14 @@ class MicroserviceSeeder extends Seeder
 
 
         foreach ($microservices as $microservice) {
-            if (!Microservice::whereName($microservice['name'])->first()) {
+            $model = Microservice::whereName($microservice['name'])->first();
+            if (!$model) {
                 Microservice::create($microservice);
+            } else {
+                $model->update([
+                    'hostname' => $microservice['hostname'],
+                    'port' => $microservice['port']
+                ]);
             }
         }
 
